e16fbbf26eb229383a9c6f117648f85dec9fabf8
Martin Andrysek <martin.andrysek13@gmail.com>
2017-04-14 20:41:56 +0200

    encrypting done

--- unsafe_RSA/encrypt.py
+++ unsafe_RSA/encrypt.py
@@ -2,7 +2,24 @@
 
 import square_and_multiply
 
-def encrypt():
-    
+from Crypto.PublicKey import RSA
+
+def encrypt(ot):
+
+    with open('../keys/public.pem') as r:
+
+        RSAkey = RSA.importKey(r.read(), '1234')
+
+    print(getattr(RSAkey.key, 'n'))
+    print(getattr(RSAkey.key, 'e'))
+
+    n = getattr(RSAkey.key, 'n')
+    e = getattr(RSAkey.key, 'e')
+    return square_and_multiply.square_and_multiply(ot, n, e)
 
 
+
+
+
+#ot = 651351613684158318684
+#print(encrypt(ot))
--- unsafe_RSA/square_and_multiply.py
+++ unsafe_RSA/square_and_multiply.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 
 
-def square_and_multiply(ot, p, q, e):
-    n = p * q
-    fi = (p - 1) * (q - 1)
+def square_and_multiply(ot, n, e):
     st = 1
     for i in "{0:b}".format(e):
         st = (st ** 2) % n
@@ -12,8 +10,10 @@ def square_and_multiply(ot, p, q, e):
     return st
 
 
-ot = 1520
-p = 43
-q = 59
-e = 13
-print(square_and_multiply(ot, p, q, e))
+#ot = 1520
+#p = 43
+#q = 59
+#e = 13
+#n = p * q
+#fi = (p - 1) * (q - 1)
+#print(square_and_multiply(ot, p, q, e))
